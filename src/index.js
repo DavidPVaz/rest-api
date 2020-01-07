0a70cab221b1655ce688a1a9784feb6f14ff28d9
David Vaz <davidzav13@gmail.com>
2020-01-07 17:38:53 +0000

    created user class to represent model. will be on memory initially

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 import { server } from './server';
 
+import { User } from './model/user';
+
 dotenv.config();
 
 const port = process.env.PORT || 8888;
@@ -8,3 +10,7 @@ const port = process.env.PORT || 8888;
 server.listen(port, () => {
     console.log(`Server has started on port ${port}...`);
 });
+
+
+
+
---
+++ src/model/user.js
@@ -0,0 +1,48 @@
+const User = (function () {
+
+    const privateProperties = new WeakMap();
+
+    class User {
+        constructor(username, email, password) {
+            privateProperties.set(this, {
+                username,
+                email,
+                password
+            });
+        }
+
+        static delete(user) {
+            privateProperties.delete(user);
+        }
+
+        getUsername() {
+            return privateProperties.get(this).username;
+        }
+
+        getEmail() {
+            return privateProperties.get(this).email;
+        }
+
+        getPassword() {
+            return privateProperties.get(this).password;
+        }
+
+        setUsername(newUsername) {
+            privateProperties.get(this).username = newUsername;
+        }
+
+        setEmail(newEmail) {
+            privateProperties.get(this).email = newEmail;
+        }
+
+        setPassword(newPassword) {
+            privateProperties.get(this).password = newPassword;
+        }
+
+    }
+
+    return User;
+
+})();
+
+export { User };
