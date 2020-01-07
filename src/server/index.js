3ef69b8acc9775f9e5da81a8a5c1c799b2f55efc
David Vaz <davidzav13@gmail.com>
2020-01-07 16:23:05 +0000

    added validation to request body with joi

--- src/server/index.js
+++ src/server/index.js
@@ -3,16 +3,29 @@ import express from 'express';
 import cors from 'cors';
 import tlsCredentials from '../../config';
 
+import { validateLoginParameters } from '../../utils/validation';
+
 const app = express();
 
+app.disable('x-powered-by');
+app.disable('etag');
+
+app.use(express.json());
 app.use(cors({
     origin: '*',
     methods: [ 'GET', 'POST', 'PUT', 'DELETE' ],
     maxAge: 3600
 }));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.post('/api', (req, res) => {
+    const { error } = validateLoginParameters(req);
+
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    return res.send("Yay");
+
 });
 
 const server = https.createServer(tlsCredentials, app);
---
+++ utils/validation/index.js
@@ -0,0 +1,16 @@
+import Joi from '@hapi/joi';
+
+export function validateLoginParameters({ body }) {
+
+    const schema = Joi.object({
+        username: Joi.string().alphanum().min(3).max(20).required(),
+        email: Joi.string().max(30).email().required(),
+        password: Joi.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[.,!@#$%^&*])(?=.{8,})/).required().error(() => {
+            const error = Error();
+            error.details = [{message: 'Password must have a minimum of 8 characters and contain at least: one lower case letter, one upper case letter, one number, one special character(.,!@#$%^&*)'}];
+            return error;
+        })
+    });
+    
+    return schema.validate(body);
+};
