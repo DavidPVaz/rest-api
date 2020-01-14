b78cde86283c40b724176bb33c5e37a425236434
David Vaz <davidzav13@gmail.com>
2020-01-14 21:20:04 +0000

    added additional function to database util to set the id in sequence after seeds

--- utils/database/schema.js
+++ utils/database/schema.js
@@ -5,9 +5,23 @@ import { knex } from '../../db/knex';
     try {
         await knex.migrate.latest();
         await knex.seed.run();
+        await setNextInDbSequence(knex);
     } catch (error) {
         console.log(error);
     } finally {
         await knex.destroy();
     }
 })();
+
+async function setNextInDbSequence(knex) {
+
+    const tables = await knex('pg_tables').select('tablename').where('schemaname', 'public');
+
+    await Promise.all(
+        tables
+            .filter(value => !value.tablename.startsWith('knex_'))
+            .map(value => knex.raw(
+                `SELECT setval('${value.tablename}_id_seq', coalesce(max(id), 0) + 1, false) FROM ${value.tablename};`
+            ))
+    );
+}
