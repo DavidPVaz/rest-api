12e4d647650c62c5a8f51285c16eab7d63a65867
David Vaz <davidzav13@gmail.com>
2020-01-20 09:18:55 +0000

    config doc

--- config/index.js
+++ config/index.js
@@ -1,11 +1,18 @@
 import fs from 'fs';
 import crypto from 'crypto';
-
+/** 
+ * TLS configuration object.
+ * 
+ * @property {string} key  - TLS key.
+ * @property {string} cert - TLS certificate.
+ */
 const tls = {
     key: fs.readFileSync('config/tls/server.key'),
     cert: fs.readFileSync('config/tls/server.crt')
 };
-
+/** 
+ * Secret used to generate JWT.
+ */
 const secret = Buffer.from(crypto.randomBytes(256).toString('base64'), 'base64');
 
 export default {
