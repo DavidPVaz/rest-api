e9a5d11b58aa77a149703c337b6fa04f670dbce2
David Vaz <davidpvaz@protonmail.com>
2022-01-05 15:09:57 +0000

    db reseting is now fully implemented

--- knexfile.js
+++ knexfile.js
@@ -24,7 +24,12 @@ const KnexConfig = {
 
     development: {
         client: 'pg',
-        connection: process.env.DB_URI,
+        connection: {
+            host: process.env.DB_HOST,
+            database: process.env.DB_NAME,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS
+        },
         pool: {
             min: 2,
             max: 10
